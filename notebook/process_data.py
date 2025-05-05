2b9d1f11803cc864c77b6345e3a1e9846e2a33e2
atsuhiro <jhuim08@gmail.com>
2025-05-05 16:43:42 +0900

    update csv data

--- notebook/process_data.py
+++ notebook/process_data.py
@@ -5,30 +5,29 @@ from IPython.display import display
 from src.consts import ROOT
 
 # %%
-# データの確認とカテゴリごとにソートして保存するだけ
+pl.Config.set_tbl_cols(50)
+pl.Config.set_tbl_rows(50)
+
+# %%
+# データの確認とカテゴリごとにソートして保存する
 # %%
 df = pl.read_csv(ROOT / "data" / "raw" / "products.csv").sort(
     by=["category", "small_category", "name", "url"]
 )  # urlはuniqueなので順番一意に定まる
-display(df)
+display(df.head())
+
 
 # %%
-# 具体小カテゴリの確認
-print(len(list(df.filter(pl.col("small_category") == "パン・シリアル")["name"])))
-print(list(df.filter(pl.col("small_category") == "パン・シリアル")["name"]))
-
-# %%
-print(df.columns)
-# %%
-df.describe()
+display(df.describe())
 # %%
 # カテゴリ・小カテゴリの確認
-print(list(df["category"].unique()))
-print(list(df["small_category"].unique()))
+display(df.group_by("category").agg(pl.count()))
+display(df.group_by(["category", "small_category"]).agg(pl.count()))
+
 
 # %%
 # セブンプレミアムの確認
-df.group_by("is_seven_premium").agg(pl.count())
+display(df.group_by("is_seven_premium").agg(pl.count()))
 
 # %%
 df["name"].value_counts().sort("count", descending=True)  # 商品名に重複あり
